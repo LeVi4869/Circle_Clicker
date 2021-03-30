851c81550f569e7cb61630c2fe8c6716fc71a895
Thanh Vinh Le <thanhvinhle3900@gmail.com>
2021-03-30 20:36:20 +0700

    Added timer and score screen

--- game_file/main.lua
+++ game_file/main.lua
@@ -6,11 +6,41 @@ physics.setGravity( 0, 50 )
 
 --Starts the game
 function StartGame()
-local CS = NumericField.text
+  local secondsLeft = TimerField.text  -- 10 minutes * 60 seconds
+  secondsLeft = secondsLeft - 1
+  -- Time is tracked in seconds; convert it to minutes and seconds
+  Minutes = math.floor( secondsLeft / 60 )
+  Seconds = secondsLeft % 60
+  StartTime = string.format( "%02d:%02d", Minutes, Seconds )
+  ClockText = display.newText( StartTime, display.contentCenterX, 90, native.systemFont, 72 )
+  ClockText:setFillColor( 0.7, 0.7, 1 )
+  function UpdateTime()
+ 
+    -- Decrement the number of seconds
+    secondsLeft = secondsLeft - 1
+    -- Time is tracked in seconds; convert it to minutes and seconds
+    Minutes = math.floor( secondsLeft / 60 )
+    Seconds = secondsLeft % 60
+ 
+    -- Make it a formatted string
+    local timeDisplay = string.format( "%02d:%02d", Minutes, Seconds )
+    display.remove(ClockText);
+    -- Update the text object
+    ClockText = display.newText( timeDisplay, display.contentCenterX, 90, native.systemFont, 72 )
+    ClockText:setFillColor( 0.7, 0.7, 1 )
+    if secondsLeft == 0 then
+      ScoreScreen();
+    end
+end
+
+CountDownTimer = timer.performWithDelay( 1000, UpdateTime, secondsLeft )
+local CS = CSField.text
 local score = 0
 display.remove(StartButton)
-display.remove(NumericField)
-display.remove(SettingsText)
+display.remove(CSField)
+display.remove(CSText)
+display.remove(TimerField)
+display.remove(TimerText)
 
 local ResetButton = display.newCircle(1100, 720, 30)
 physics.addBody( ResetButton, "static")
@@ -29,7 +59,7 @@ end
 local function accurate()
   if Cooldown == false then
     local xCoord = math.random( 20, 1000 )
-    local yCoord = math.random( 100, 650 )
+    local yCoord = math.random( 150, 650 )
     transition.to( Circle, { time=0, x=xCoord, y=yCoord } )
     score = score + 1
     display.remove(Score)
@@ -39,12 +69,26 @@ local function accurate()
   timer.performWithDelay(200, endCooldown);
 end
 
+function ScoreScreen()  
+  display.remove(Circle);
+  display.remove(ResetButton);
+  display.remove(Instructions);
+  display.remove(Score);
+  display.remove(ClockText);
+  local result = string.format( "Final Score: %02d", score )
+  ResultScoreText = display.newText( result, display.contentCenterX, 200, native.systemFont, 50 )
+  MenuButton = display.newImage("Menu_Button.png", display.contentCenterX, 400);
+  MenuButton:addEventListener("touch", StartScreen)
+end
+
 function Reset()
   score = 0
   display.remove(Circle);
   display.remove(ResetButton);
   display.remove(Instructions);
   display.remove(Score);
+  display.remove(ClockText);
+  timer.cancel(CountDownTimer);
   StartScreen();
 end
 
@@ -54,16 +98,24 @@ Circle:addEventListener( "touch", accurate)
 end
 
 function StartScreen()
+  display.remove(ResultScoreText);
+  display.remove(MenuButton);
   StartButton = display.newImage( "Capture.png", 500, 200)
   StartButton:addEventListener("touch", StartGame)
-  NumericField = native.newTextField( 500, 400, 100, 50 )
-  NumericField.inputType = "decimal";
-  SettingsText = display.newText( "CircleSize:", 300, 400, native.systemFont, 28 )
+  CSField = native.newTextField( 500, 400, 100, 50 )
+  CSField.inputType = "decimal";
+  CSText = display.newText( "CircleSize:", 300, 400, native.systemFont, 28 )
+  TimerField = native.newTextField( 500, 500, 100, 50 )
+  TimerField.inputType = "decimal";
+  TimerText = display.newText( "Time Allowed:", 300, 500, native.systemFont, 28 )
 end
 
 Cooldown = false;
 StartButton = display.newImage( "Capture.png", 500, 200)
 StartButton:addEventListener("touch", StartGame)
-NumericField = native.newTextField( 500, 400, 100, 50 )
-NumericField.inputType = "decimal";
-SettingsText = display.newText( "CircleSize:", 300, 400, native.systemFont, 28 )
+CSField = native.newTextField( 500, 400, 100, 50 )
+CSField.inputType = "decimal";
+CSText = display.newText( "CircleSize:", 300, 400, native.systemFont, 28 )
+TimerField = native.newTextField( 500, 500, 100, 50 )
+TimerField.inputType = "decimal";
+TimerText = display.newText( "Time Allowed:", 300, 500, native.systemFont, 28 )
