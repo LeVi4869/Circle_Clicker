008b209c1d02574923376e8646ba50514ad4e6fd
Thanh Vinh Le <thanhvinhle3900@gmail.com>
2021-04-05 00:50:40 +0700

    Added accuracy tracker and the game build

---
+++ GameSource/main.lua
@@ -0,0 +1,159 @@
+--Initialization
+display.setDefault( "background", 0, 0.3, 0.8 )
+local physics = require("physics")
+physics.start()
+physics.setGravity( 0, 50 )
+--Starts the game
+function StartGame()
+  InGame = true;
+  local secondsLeft = TimerField.text  -- 10 minutes * 60 seconds
+  secondsLeft = secondsLeft - 1
+  -- Time is tracked in seconds; convert it to minutes and seconds
+  Minutes = math.floor( secondsLeft / 60 )
+  Seconds = secondsLeft % 60
+  StartTime = string.format( "%02d:%02d", Minutes, Seconds )
+  ClockText = display.newText( StartTime, display.contentCenterX, 90, native.systemFont, 72 )
+  ClockText:setFillColor( 0.7, 0.7, 1 )
+
+  function UpdateTime()
+ 
+    -- Decrement the number of seconds
+    secondsLeft = secondsLeft - 1
+    -- Time is tracked in seconds; convert it to minutes and seconds
+    Minutes = math.floor( secondsLeft / 60 )
+    Seconds = secondsLeft % 60
+ 
+    -- Make it a formatted string
+    local timeDisplay = string.format( "%02d:%02d", Minutes, Seconds )
+    display.remove(ClockText);
+    -- Update the text object
+    ClockText = display.newText( timeDisplay, display.contentCenterX, 90, native.systemFont, 72 )
+    ClockText:setFillColor( 0.7, 0.7, 1 )
+    if secondsLeft == 0 then
+      ScoreScreen();
+    end
+  end
+
+  Clicks = -1;
+  Hits = 0;
+
+  Accuracy = string.format( "0.00" )
+  
+  local function endClickCooldown()
+    ClickCooldown = false;
+  end
+
+  local function OnMouseEvent()
+    if InGame == true then
+      if ClickCooldown == false then
+        Clicks = Clicks + 1;
+        display.remove(AccuracyText);
+        if Clicks == 0 then
+          Accuracy = string.format( "100.00" )
+        else
+          Accuracy = string.format( "%.2f", (Hits/Clicks)*100 )
+        end
+        AccuracyText = display.newText( Accuracy, 800, 90, native.systemFont, 40 )
+        ClickCooldown = true;
+      end
+      timer.performWithDelay(100, endClickCooldown);
+    end
+  end
+              
+  -- Add the mouse event listener.
+  Runtime:addEventListener("touch", OnMouseEvent )
+
+  CountDownTimer = timer.performWithDelay( 1000, UpdateTime, secondsLeft )
+  local CS = CSField.text
+  local score = 0
+  display.remove(StartButton)
+  display.remove(CSField)
+  display.remove(CSText)
+  display.remove(TimerField)
+  display.remove(TimerText)
+
+  local ResetButton = display.newCircle(970, 700, 30)
+  physics.addBody( ResetButton, "static")
+  ResetButton:setFillColor( 0.5 )
+
+  Circle = display.newCircle( 500, 300, 160/CS);
+
+  Instructions = display.newText( "Tap the bottom right button to reset", display.contentCenterX, 40, native.systemFont, 28 )
+  Instructions:setFillColor( 1 )
+  Score = display.newText( score, 900, 40, native.systemFont, 28 )
+
+  local function endAccurateCooldown()
+    AccurateCooldown = false;
+  end
+
+  local function accurate()
+    if AccurateCooldown == false then
+      local xCoord = math.random( 60, 960 )
+      local yCoord = math.random( 150, 650 )
+      transition.to( Circle, { time=0, x=xCoord, y=yCoord } )
+      score = score + 1
+      Hits = Hits + 1;
+      display.remove(Score)
+      Score = display.newText( score, 900, 40, native.systemFont, 28 )
+      AccurateCooldown = true;
+    end
+    timer.performWithDelay(100, endAccurateCooldown);
+  end
+
+  function ScoreScreen()  
+    display.remove(Circle);
+    display.remove(ResetButton);
+    display.remove(Instructions);
+    display.remove(Score);
+    display.remove(AccuracyText);
+    display.remove(ClockText);
+    display.remove(AccuracyText);
+    InGame = false;
+    local result = string.format( "Final Score: %02d", score )
+    ResultScoreText = display.newText( result, display.contentCenterX, 200, native.systemFont, 50 )
+    local resultAcc = string.format("Accuracy: %s", Accuracy)
+    ResultAccText = display.newText( resultAcc, display.contentCenterX, 300, native.systemFont, 50 )
+    MenuButton = display.newImage("Menu_Button.png", display.contentCenterX, 400);
+    MenuButton:addEventListener("touch", StartScreen)
+  end
+
+  function Reset()
+    score = 0
+    display.remove(Circle);
+    display.remove(ResetButton);
+    display.remove(Instructions);
+    display.remove(Score);
+    display.remove(AccuracyText);
+    display.remove(ClockText);
+    timer.cancel(CountDownTimer);
+    StartScreen();
+  end
+
+  ResetButton:addEventListener( "touch", Reset)
+  Circle:addEventListener( "touch", accurate)
+
+end
+
+function StartScreen()
+  display.remove(ResultScoreText);
+  display.remove(ResultAccText);
+  display.remove(MenuButton);
+  InGame = false;
+  StartButton = display.newImage( "Capture.png", 500, 200)
+  StartButton:addEventListener("touch", StartGame)
+  CSField = native.newTextField( 500, 400, 100, 50 )
+  CSField.inputType = "decimal";
+  CSText = display.newText( "CircleSize:", 300, 400, native.systemFont, 28 )
+  TimerField = native.newTextField( 500, 500, 100, 50 )
+  TimerField.inputType = "decimal";
+  TimerText = display.newText( "Time Allowed:", 300, 500, native.systemFont, 28 )
+end
+
+StartButton = display.newImage( "Capture.png", 500, 200)
+  StartButton:addEventListener("touch", StartGame)
+  CSField = native.newTextField( 500, 400, 100, 50 )
+  CSField.inputType = "decimal";
+  CSText = display.newText( "CircleSize:", 300, 400, native.systemFont, 28 )
+  TimerField = native.newTextField( 500, 500, 100, 50 )
+  TimerField.inputType = "decimal";
+  TimerText = display.newText( "Time Allowed:", 300, 500, native.systemFont, 28 )
